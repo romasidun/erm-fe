c730ac33f69d5bb84afe1918ad6390010637afa2
ole-huv <ole.hub@outlook.com>
2017-02-02 20:25:36 +0200

    change testplan

--- public/assets/js/controllers/control/testplan.update.ctrl.js
+++ public/assets/js/controllers/control/testplan.update.ctrl.js
@@ -39,12 +39,12 @@
             });
         };
 
-        $scope.cancelAction = function() {
-            if($scope.Form.TestPlan.$dirty) {
-                var confirm = Utils.CreateConfirmModal("Confirmation", "Are you sure you want to cancel?", "Yes", "No");
-                confirm.result.then(function(){ $state.go('app.control.testplan.main'); });
-                return false;
-            }
+        $scope.cancelActions = function() {
+            // if($scope.Form.TestPlan.$dirty) {
+            //     var confirm = Utils.CreateConfirmModal("Confirmation", "Are you sure you want to cancel?", "Yes", "No");
+            //     confirm.result.then(function(){ $state.go('app.control.testplan.main'); });
+            //     return false;
+            // }
             $state.go('app.control.testplan.main');
         };
 
