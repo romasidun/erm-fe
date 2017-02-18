c6f8a61ad9b00c7fa6c224e475d11bbe0912388f
roma2017 <romasidun@outlook.com>
2017-02-18 16:54:06 +0200

    added vendor email

--- public/assets/js/controllers/vendorrisk/vendorrisk.stinfo.update.ctrl.js
+++ public/assets/js/controllers/vendorrisk/vendorrisk.stinfo.update.ctrl.js
@@ -1,15 +1,15 @@
-(function(){
-    VendorStinfoUpdateController.inject = ['$scope','$rootScope','$state', 'VendorService', 'Utils'];
-    app.controller('VendorStinfoUpdateCtrl',VendorStinfoUpdateController);
-    function VendorStinfoUpdateController($scope, $rootScope, $state, VendorService, Utils){
+(function () {
+    VendorStinfoUpdateController.inject = ['$scope', '$rootScope', '$state', 'VendorService', 'Utils'];
+    app.controller('VendorStinfoUpdateCtrl', VendorStinfoUpdateController);
+    function VendorStinfoUpdateController($scope, $rootScope, $state, VendorService, Utils) {
         $scope.mainTitle = $state.current.title;
         $scope.mainDesc = "VENDOR UPDATE PAGE";
         $scope.showExcelButton = true;
 
-        $scope.cancelAction = function(){
-            if($scope.Form.VendorRisk.$dirty){
+        $scope.cancelAction = function () {
+            if ($scope.Form.VendorRisk.$dirty) {
                 var confirm = Utils.CreateConfirmModal("Confirmation", "Are you sure you want to cancel?", "Yes", "No");
-                confirm.result.then(function(){
+                confirm.result.then(function () {
                     $state.go('app.vendorrisk.stinfo.main');
                 });
                 return false;
@@ -17,35 +17,57 @@
             $state.go('app.vendorrisk.stinfo.main');
         };
 
-        VendorService.GetRimById($state.params.id).then(function(data){
-           data.approvedDate = new Date(data.approvedDate);
-           $scope.approvedDate = Utils.GetDPDate(data.approvedDate);
-           $scope.assessmentsDate = Utils.GetDPDate(data.assessmentsDate);
-           $scope.VM = data;
-           $rootScope.app.Mask = false;
-        });
-
-        $scope.submitAction = function() {
-            if($scope.Form.VendorRisk.$invalid) return false;
+        $scope.submitAction = function () {
+            if ($scope.Form.VendorRisk.$invalid) return false;
             VendorService.UpdateRam($state.params.id, $scope.VM).then(function (res) {
-                if(res.status===200) {
+                if (res.status === 200) {
                     $rootScope.app.Mask = false;
                     $state.go('app.vendorrisk.stinfo.main');
                 }
             });
         };
-
-        $scope.createAssessmentAction = function(){
-            $state.go('app.vendorrisk.assessment',{id: $state.params.id});
+        $scope.createAssessmentAction = function () {
+            $state.go('app.vendorrisk.assessment', {id: $state.params.id});
         };
 
-        VendorService.GetRiskType().then(function(risktype){
+        VendorService.GetRimById($state.params.id).then(function (data) {
+            data.approvedDate = new Date(data.approvedDate);
+            $scope.approvedDate = Utils.GetDPDate(data.approvedDate);
+            $scope.assessmentsDate = Utils.GetDPDate(data.assessmentsDate);
+            $scope.VM = data;
+            $rootScope.app.Mask = false;
+        });
+
+        VendorService.GetRiskType().then(function (risktype) {
             $scope.riskTypeList = risktype;
         });
 
-        VendorService.GetVendor().then(function(vendor){
+        $scope.Grid1 = {
+            Column: 'vendorName',
+            IsAsc: true,
+            Data: [],
+            SortMe: function (col) {
+                if ($scope.Grid1.Column === col)
+                    $scope.Grid1.IsAsc = !$scope.Grid1.IsAsc;
+                else
+                    $scope.Grid1.Column = col;
+            },
+            GetIco: function (col) {
+                if ($scope.Grid1.Column === col) {
+                    return $scope.Grid1.IsAsc ? 'fa-sort-up' : 'fa-sort-down';
+                } else {
+                    return 'fa-unsorted';
+                }
+            }
+        };
+        VendorService.GetVendor().then(function (vendor) {
+            $scope.Grid1.Data = vendor;
             $scope.vendor = vendor;
         });
+
+        $scope.sendMail = function () {
+            
+        }
     }
 
 })();
