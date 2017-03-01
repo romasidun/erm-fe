1cb71e86c599689c906d81ea546cbcc5b4a90ea8
roma2017 <romasidun@outlook.com>
2017-03-02 00:37:49 +0200

    multivendor headmap

--- public/assets/js/controllers/vendorrisk/vendorrisk.scorecard.ctrl.js
+++ public/assets/js/controllers/vendorrisk/vendorrisk.scorecard.ctrl.js
@@ -100,7 +100,7 @@
             });
 
             //MultiVendor Chart load
-            /*setupMultiVendChart(data);*/
+            setupMultiVendChart(data);
 
             $scope.riskDataOrigin = data;
 
@@ -114,7 +114,7 @@
             if(nv) setupVendorChart(nv);
         });
 
-        /*function setupMultiVendChart(data){
+        function setupMultiVendChart(data){
             var XCategories = ['overallRiskWeight', 'overallFindingWeight'];
             var ChartOpts = {
                 Title: 'Multi-Vendor Risk  Ratings',
@@ -146,7 +146,7 @@
             console.log(ChartOpts);
 
             ChartFactory.BuildHeatMap('multiVendHeatMap', ChartOpts);
-        }*/
+        }
 
         function setupVendorChart(vend){
             var XCategories = ['responsesNumYes', 'responsesNumNo', 'responsesNumNA', 'findingTotal', 'overallRiskWeight', 'overallFindingWeight'];
